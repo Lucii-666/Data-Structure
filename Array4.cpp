f5567b793b74565e2bc586f8ca58f34b2890352f
Lucii-666 <ashutoshhjp1067@gmail.com>
2024-08-16 12:53:26 +0530

    Remove duplicates from Sorted array

---
+++ Array4.cpp
@@ -0,0 +1,24 @@
+//Remove duplicates from Sorted array
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    int num[] = {1, 2, 2, 3, 4, 4, 5}; 
+    int n = sizeof(num) / sizeof(num[0]); 
+    if (n == 0) {
+        return 0; 
+    }
+    int j = 0;
+    for (int i = 1; i < n; i++) {
+        if (num[i] != num[j]) { 
+            j++;
+            num[j] = num[i]; 
+        }
+    }
+    for (int i = 0; i <= j; i++) {
+        cout << num[i] << " ";
+    }
+    cout << endl;
+    //return j;// + 1;
+}
